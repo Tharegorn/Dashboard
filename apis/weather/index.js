f0b0814d4ee9735b63733439cfe0481830ceaa0b
Jamie <jamie-antoine.ponchon@epitech.eu>
2021-08-10 23:04:56 +0200

    add weather api without call back

--- apis/weather/index.js
+++ apis/weather/index.js
@@ -1,7 +1,22 @@
+var axios = require("axios").default;
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const app = express();
+
+var options = {
+  method: 'GET',
+  url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  params: {
+    q: 'qsdqs',
+    lang: 'fr_fr',
+    units: 'metric',
+  },
+  headers: {
+    'x-rapidapi-key': '990cf10511msh64a893edd2e1083p145fb6jsn72ba086c0b1a',
+    'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com'
+  }
+};
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -15,16 +30,12 @@ app.listen(4243, () => {
   console.log("Listening on port 4243.");
 });
 
-app.get('*', (req, res) => {
-  res.send("Bite")
-})
-
-app.post("/weather", (req, res) => {
-  console.log("jij")
+app.post("/weather", (res, req) => {
     res.set("Content-Type", "application/json");
-    res.status(200).json({
-        status: "succes",
-        code: 200,
-        data: { message: "Success", value: "fait chaud pd"},
-    });
+    
 })
+axios.request(options).then(function (res) {
+	console.log("City :" + res.data.name, "Temp:" + res.data.main.temp, "Desc: " + res.data.weather[0].description);
+}).catch(function (error) {
+    console.log("fdp")
+});
