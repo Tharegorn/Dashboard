59b912219f0bf52ff992142e9d1b1e184d68834e
Jamie <jamie-antoine.ponchon@epitech.eu>
2021-08-12 01:37:59 +0200

    add requests for weather api

--- apis/weather/index.js
+++ apis/weather/index.js
@@ -2,7 +2,7 @@ var axios = require("axios").default;
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const app = express()
 var options = {
   method: 'GET',
   url: 'https://community-open-weather-map.p.rapidapi.com/weather',
@@ -30,12 +30,19 @@ app.listen(4243, () => {
   console.log("Listening on port 4243.");
 });
 
-app.post("/weather", (res, req) => {
+app.post("/weather", (req, res) => {
+  // axios.request(options).then(function (res) {
+// 	console.log("City :" + res.data.name, "Temp:" + res.data.main.temp, "Desc: " + res.data.weather[0].description);
+// }).catch(function (error) {
+//     console.log("fdp")
+// });
     res.set("Content-Type", "application/json");
+    console.log(req.body.data.City)
+    if (req.body.data.City == "Paris")
+      res.status(200).json({status: "Succes", code: 200,city: req.body.data.City, temp: "Unknow", desc: "Unknow"})
+    else
+      res.status(500).json({status: "Failure", code: 500,city: req.body.data.City, msg: "Unknow City"})
+
+
     
 })
-axios.request(options).then(function (res) {
-	console.log("City :" + res.data.name, "Temp:" + res.data.main.temp, "Desc: " + res.data.weather[0].description);
-}).catch(function (error) {
-    console.log("fdp")
-});
