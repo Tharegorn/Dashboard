5a3868ed1bccf19e917b32eddf29c14c3aea8cdd
Jamie <jamie-antoine.ponchon@epitech.eu>
2021-08-12 23:56:50 +0200

    finish weather api

--- apis/weather/index.js
+++ apis/weather/index.js
@@ -3,19 +3,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express()
-var options = {
-  method: 'GET',
-  url: 'https://community-open-weather-map.p.rapidapi.com/weather',
-  params: {
-    q: 'qsdqs',
-    lang: 'fr_fr',
-    units: 'metric',
-  },
-  headers: {
-    'x-rapidapi-key': '990cf10511msh64a893edd2e1083p145fb6jsn72ba086c0b1a',
-    'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com'
-  }
-};
+
+function options(c) {
+  return ({
+    method: 'GET',
+    url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+    params: {
+      q: c,
+      lang: 'en_en',
+      units: 'metric',
+    },
+    headers: {
+      'x-rapidapi-key': '990cf10511msh64a893edd2e1083p145fb6jsn72ba086c0b1a',
+      'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com'
+    }
+  })
+}
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,18 +34,14 @@ app.listen(4243, () => {
 });
 
 app.post("/weather", (req, res) => {
-  // axios.request(options).then(function (res) {
-// 	console.log("City :" + res.data.name, "Temp:" + res.data.main.temp, "Desc: " + res.data.weather[0].description);
-// }).catch(function (error) {
-//     console.log("fdp")
-// });
-    res.set("Content-Type", "application/json");
-    console.log(req.body.data.City)
-    if (req.body.data.City == "Paris")
-      res.status(200).json({status: "Succes", code: 200,city: req.body.data.City, temp: "Unknow", desc: "Unknow"})
-    else
-      res.status(500).json({status: "Failure", code: 500,city: req.body.data.City, msg: "Unknow City"})
-
-
-    
+  res.set("Content-Type", "application/json");
+  if (req.body.data.City) {
+    axios.request(options(req.body.data.City)).then(function (rev) {
+      res.status(200).json({ status: "Succes", code: 200, city: rev.data.name, temp: rev.data.main.temp, desc: rev.data.weather[0].description })
+    }).catch(function (error) {
+      res.status(500).json({ status: "Failure", code: 500, msg: "Unknow City" })
+    });
+  } else {
+    res.status(500).json({ status: "Failure", code: 500, msg: "Unknow City" })
+  }
 })
