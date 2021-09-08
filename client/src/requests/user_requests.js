61c82ad04b2df2b4d284a3258b7dc219d34273f4
Jamie <jamie-antoine.ponchon@epitech.eu>
2021-09-08 23:25:44 +0200

    add all user requests for user modification and auth

--- client/src/requests/user_requests.js
+++ client/src/requests/user_requests.js
@@ -24,9 +24,49 @@ function verify(token) {
     })
 }
 
-function checkAdmin(token) {
+function checkAdmin(name) {
     return new Promise(function (resolve, reject) {
-        axios.post('http://localhost:4242/adminLoadUsers', { token: token }).then(res => {
+        axios.post('http://localhost:4242/loadusers', { name: name }).then(res => {
+            resolve(res)
+        }).catch((err) => setImmediate(() => { reject(err) }))
+    })
+}
+
+function deleteUser(id) {
+    return new Promise(function (resolve, reject) {
+        axios.post('http://localhost:4242/delete', { id: id }).then(res => {
+            resolve(res)
+        }).catch((err) => setImmediate(() => { reject(err) }))
+    })
+}
+
+function promove(id, perm) {
+    return new Promise(function (resolve, reject) {
+        axios.post('http://localhost:4242/promove', { id: id, perm: perm }).then(res => {
+            resolve(res)
+        }).catch((err) => setImmediate(() => { reject(err) }))
+    })
+}
+
+function check_jwt(token) {
+    var base64Url = token.split('.')[1];
+    if (!base64Url) return ("INVALID");
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    var result = JSON.parse(jsonPayload)
+    if (!result.name)
+        return ("ERROR")
+    if (result.perm === 1)
+        return ("adm")
+    else
+        return ("normal");
+}
+
+function check_token(token) {
+    return new Promise(function (resolve, reject) {
+        axios.post('http://localhost:4242/token', { token: token}).then(res => {
             resolve(res)
         }).catch((err) => setImmediate(() => { reject(err) }))
     })
@@ -36,4 +76,8 @@ export {
     loginUser,
     verify,
     checkAdmin,
+    check_jwt,
+    deleteUser,
+    promove,
+    check_token,
 }
