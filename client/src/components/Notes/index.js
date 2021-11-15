caa2968cff232566c0f40a310b6c3ea4d292c85d
Jamie <jamie-antoine.ponchon@epitech.eu>
2021-11-15 22:04:51 +0100

    add node visual and handle edit

--- client/src/components/Notes/index.js
+++ client/src/components/Notes/index.js
@@ -1,16 +1,56 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react";
 import { getNotes } from "../../requests/user_requests";
-import jwt_decode from "jwt-decode"
+import jwt_decode from "jwt-decode";
+import "./index.css";
+import Draggable from "react-draggable";
+
 function Notes() {
-    const [notes, setNotes] = useState(null)
+  const [notes, setNotes] = useState(null);
+  function retrieve() {
     getNotes(jwt_decode(localStorage.getItem("session_id")).id).then((res) => {
-        setNotes(res.data.content)
-    })
-    return (
-        <>
-            {notes ? notes.map((item, index) => (<div key={index}><div>{item.title} {item.content}</div></div>)) : <div>no</div>}
-        </>
-    )
+      setNotes(res.data.content);
+    });
+  }
+  useEffect(() => {
+    retrieve();
+  }, []);
+
+  return (
+    <>
+      <div
+        onClick={() => {
+          retrieve();
+        }}
+      >
+        Refresh
+      </div>
+      {notes ? (
+        <ul>
+          {notes.map((item, index) => (
+            <Draggable key={index} handle=".handle">
+              <li className="handle">
+                <div className="card">
+                  <h2
+                    contentEditable
+                    onInput={(e) => {
+                        console.log(e.target.childNodes[0], item.id, "title")
+                    }}
+                  >
+                    {item.title}
+                  </h2>
+                  <p contentEditable onInput={(e) => {
+                        console.log(e.target.childNodes[0], item.id, "content")
+                    }}>{item.content}</p>
+                </div>
+              </li>
+            </Draggable>
+          ))}
+        </ul>
+      ) : (
+        <div>no</div>
+      )}
+    </>
+  );
 }
 
 export default Notes;
